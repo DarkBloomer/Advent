a1a5e84ac6533508d6f2da09aa3f9db475e26c1f
DarkBloomer <cameronfitz98@gmail.com>
2023-01-23 19:54:41 -0500

    updating

--- Day5/AdventDay5/stack.py
+++ Day5/AdventDay5/stack.py
@@ -10,12 +10,38 @@ lines = f.readlines()
 # Close the file
 f.close()
 
-for line in lines:
-	if 'move' in line:
-		l = line.split(' ')
-		l[5] = l[5].strip('\n')
-		l.pop(0)
-		l.pop(1)
-		l.pop(2)
-		print(l)	
+def formatGame(gameLines):
+	rowCnt = 0
+	rows = []
+	rules = []
+
+	for line in gameLines:
+		if line == '\n':
+			continue
+
+		elif '[' in line:
+			rows.append(line)
+			rowCnt += 1
+
+		elif line[1] == '1':
+			cols = line.split('   ')
+			cols[0] = '1'
+			cols[len(cols) - 1] = cols[len(cols) - 1].replace(' \n', '')
+			print(cols)
+
+		elif 'move' in line:
+			l = line.split(' ')
+			l[5] = l[5].strip('\n')
+			l.pop(0)
+			l.pop(1)
+			l.pop(2)
+			rules.append(l)
+
+		print("RULES")
+		for rule in rules:
+			print(rule)
 		
+		print("ROWS")
+		for r in rows:
+			print(r)
+formatGame(lines)
